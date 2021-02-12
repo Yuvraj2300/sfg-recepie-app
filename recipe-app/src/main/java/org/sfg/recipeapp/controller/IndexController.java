ea1f444455cb5669926b41045ae87aba2b7a84c3
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-02-12 15:30:03 +0530

    Init

---
+++ recipe-app/src/main/java/org/sfg/recipeapp/controller/IndexController.java
@@ -0,0 +1,10 @@
+package org.sfg.recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+@RequestMapping("/recipes")
+public class IndexController {
+
+}
