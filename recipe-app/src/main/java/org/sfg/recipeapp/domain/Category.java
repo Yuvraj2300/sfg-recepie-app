02c568e34b444f157d86a3d896ba18edf67e11fd
yuvraj <yuvraj.sharma@statusneo.com>
2021-04-12 15:57:36 +0530

    Completed the domain implementation with Join columns as well

---
+++ recipe-app/src/main/java/org/sfg/recipeapp/domain/Category.java
@@ -0,0 +1,23 @@
+package org.sfg.recipeapp.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String description;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Recipe> recepies;
+}
---
+++ recipe-app/src/main/java/org/sfg/recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package org.sfg.recipeapp.domain;
+
+public enum Difficulty {
+	EASY, MODERATE, KIND_OF_HARD, HARD
+}
