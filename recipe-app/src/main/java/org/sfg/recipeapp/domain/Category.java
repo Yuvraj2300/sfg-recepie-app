17fd8b79157d1c3281b73c6dc10edbff59f9b723
yuvraj <yuvraj.sharma@statusneo.com>
2021-04-14 22:31:57 +0530

    Added some data and fixed a generation property :zap:

--- recipe-app/src/main/java/org/sfg/recipeapp/domain/Category.java
+++ recipe-app/src/main/java/org/sfg/recipeapp/domain/Category.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Entity
 public class Category {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String description;
 
