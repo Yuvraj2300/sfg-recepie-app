dd5a9d5b7f591fb656712dc01ecc5d11b78f47f7
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2021-06-25 17:06:47 +0530

    Dealing with commands in the service layer

--- recipe-app/src/main/java/org/sfg/recipeapp/service/RecipeServiceImpl.java
+++ recipe-app/src/main/java/org/sfg/recipeapp/service/RecipeServiceImpl.java
@@ -4,18 +4,32 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import org.sfg.recipeapp.commands.RecipeCommand;
+import org.sfg.recipeapp.converters.RecipeCommandToRecipe;
+import org.sfg.recipeapp.converters.RecipeToRecipeCommand;
 import org.sfg.recipeapp.domain.Recipe;
 import org.sfg.recipeapp.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
 	private RecipeRepository recipeRepo;
+	private final RecipeCommandToRecipe recipeCommandToRecipe;
+	private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-	public RecipeServiceImpl(RecipeRepository repo) {
-		this.recipeRepo = repo;
+
+	public RecipeServiceImpl(RecipeRepository recipeRepo, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
+		super();
+		this.recipeRepo = recipeRepo;
+		this.recipeCommandToRecipe = recipeCommandToRecipe;
+		this.recipeToRecipeCommand = recipeToRecipeCommand;
 	}
+
+
 
 	@Override
 	public Set<Recipe> getRecipes() {
@@ -39,5 +53,14 @@ public class RecipeServiceImpl implements RecipeService {
 		}
 
 		return recipeOptional.get();
+	}
+
+	@Override
+	public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+		Recipe deatachedRecipe = recipeCommandToRecipe.convert(command);
+
+		Recipe savedRecipe = recipeRepo.save(deatachedRecipe);
+		log.debug("Saved recipeId : " + savedRecipe.getId());
+		return recipeToRecipeCommand.convert(savedRecipe);
 	}
 }
