9488b59f19e39ea366da1f406eca2530bd1d21dd
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2021-06-25 16:53:26 +0530

    Command to domain converters and viceversa

---
+++ recipe-app/src/main/java/org/sfg/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -0,0 +1,31 @@
+package org.sfg.recipeapp.converters;
+
+
+import org.sfg.recipeapp.commands.UnitOfMeasureCommand;
+import org.sfg.recipeapp.domain.UnitOfMeasure;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import lombok.Synchronized;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+	@Synchronized
+	@Nullable
+	@Override
+	public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+		if (unitOfMeasure != null) {
+			final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+			uomc.setId(unitOfMeasure.getId());
+			uomc.setDescription(unitOfMeasure.getDescription());
+			return uomc;
+		}
+		return null;
+	}
+}
