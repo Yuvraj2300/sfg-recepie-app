b986b4e0015a8161756fd63304f5ded31b5b72f6
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2021-06-26 16:12:08 +0530

    Creation feature

--- recipe-app/src/main/java/org/sfg/recipeapp/controller/IndexController.java
+++ recipe-app/src/main/java/org/sfg/recipeapp/controller/IndexController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class IndexController {
 
 	RecipeService service;
@@ -17,6 +20,7 @@ public class IndexController {
 
 	@RequestMapping({ "", "/", "/index" })
 	public String getIndexPage(Model model) {
+		log.info("getting the landing page");
 		model.addAttribute("recipes", service.getRecipes());
 
 		return "index";
--- recipe-app/src/main/java/org/sfg/recipeapp/controller/RecipeController.java
+++ recipe-app/src/main/java/org/sfg/recipeapp/controller/RecipeController.java
@@ -1,12 +1,18 @@
 package org.sfg.recipeapp.controller;
 
+import org.sfg.recipeapp.commands.RecipeCommand;
 import org.sfg.recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class RecipeController {
 
 	private final RecipeService recipeService;
@@ -15,9 +21,40 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 
-	@RequestMapping("/recipe/show/{id}")
+
+
+
+	@RequestMapping(value = "recipe/show/{id}", method = { RequestMethod.GET, RequestMethod.GET })
 	public String showById(@PathVariable String id, Model model) {
+		log.info("trying to show recipe with id {}", id);
 		model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 		return "recipe/show";
 	}
+
+
+
+
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		log.info("trying to save new model {}", model);
+		model.addAttribute("recipe", new RecipeCommand());
+		log.info("model updated {}", model);
+		return "recipe/recipeform";
+	}
+
+
+
+	/**
+	 * @param command
+	 * @return
+	 * @implNote So this returns a redirect once the post is completed!
+	 */
+	@RequestMapping(value = "recipe", method = { RequestMethod.GET, RequestMethod.GET })
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		log.info("trying to save new recipe ::: {}", command.toString());
+		RecipeCommand saveRecipe = recipeService.saveRecipeCommand(command);
+
+		return "redirect:/recipe/show/" + saveRecipe.getId();
+	}
+
 }
