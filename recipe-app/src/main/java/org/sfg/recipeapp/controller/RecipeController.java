edb47cad58bdb7e5727a0416411e7bb83adfeac5
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2021-10-18 15:31:01 +0530

    added exception to the 404 page

--- recipe-app/src/main/java/org/sfg/recipeapp/controller/RecipeController.java
+++ recipe-app/src/main/java/org/sfg/recipeapp/controller/RecipeController.java
@@ -81,10 +81,11 @@ public class RecipeController {
 
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(NotFoundException.class)
-	ModelAndView handleNotFound() {
-		log.error("Handling not found exception");
+	ModelAndView handleNotFound(Exception e) {
+		log.error(e.getMessage());
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("404error");
+		modelAndView.addObject("exception", e);
 
 		return modelAndView;
 	}
